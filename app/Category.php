ba7e600fc610e3624ba68dcc86a1aeabd71cfc2f
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-05-23 15:22:33 +0200

    relazione tra post e categoria

--- app/Category.php
+++ app/Category.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    public $timestamps = false;
+
+    public function posts() {
+        return $this->hasMany('App\Post');
+    }
 }
