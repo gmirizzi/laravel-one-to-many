66c3773c35169eb3c7d39305090c2330ea39fce7
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-05-23 18:32:25 +0200

    creata navbar component

--- public/js/app.js
+++ public/js/app.js
@@ -49938,38 +49938,3 @@ module.exports = __webpack_require__(/*! C:\Users\Giuseppe Mirizzi\Documents\Boo
 /***/ })
 
 /******/ });
-
-var btnSlugger = document.querySelector('#btn-slugger');
-
-if (btnSlugger) {
-  btnSlugger.addEventListener('click', function () {
-    var eleSlug = document.querySelector('#slug');
-    var title = document.querySelector('#title').value;
-    axios.post('/admin/slugger', {
-      originalStr: title
-    }).then(function (response) {
-      eleSlug.value = response.data.slug;
-    });
-  });
-}
-
-const confirmationOverlay = document.querySelector('#confirmation-overlay');
-const confirmationForm = confirmationOverlay.querySelector('form');
-
-if (confirmationOverlay) {
-    document.querySelectorAll('.btn-delete').forEach(button => {
-        button.addEventListener('click', function() {
-            const id = this.closest('tr').dataset.id;
-            const strAction = confirmationForm.dataset.base.replace('*****', id);
-            confirmationForm.action = strAction;
-            confirmationOverlay.classList.remove('d-none');
-        })
-    });
-
-    const btnNo = document.querySelector('#btn-no');
-    btnNo.addEventListener('click', function() {
-        confirmationForm.action = '';
-        confirmationOverlay.classList.add('d-none');
-    });
-
-}
--- resources/js/app.js
+++ resources/js/app.js
@@ -30,3 +30,38 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 const app = new Vue({
     el: '#app',
 });
+
+var btnSlugger = document.querySelector('#btn-slugger');
+
+if (btnSlugger) {
+  btnSlugger.addEventListener('click', function () {
+    var eleSlug = document.querySelector('#slug');
+    var title = document.querySelector('#title').value;
+    axios.post('/admin/slugger', {
+      originalStr: title
+    }).then(function (response) {
+      eleSlug.value = response.data.slug;
+    });
+  });
+}
+
+const confirmationOverlay = document.querySelector('#confirmation-overlay');
+const confirmationForm = confirmationOverlay.querySelector('form');
+
+if (confirmationOverlay) {
+    document.querySelectorAll('.btn-delete').forEach(button => {
+        button.addEventListener('click', function() {
+            const id = this.closest('tr').dataset.id;
+            const strAction = confirmationForm.dataset.base.replace('*****', id);
+            confirmationForm.action = strAction;
+            confirmationOverlay.classList.remove('d-none');
+        })
+    });
+
+    const btnNo = document.querySelector('#btn-no');
+    btnNo.addEventListener('click', function() {
+        confirmationForm.action = '';
+        confirmationOverlay.classList.add('d-none');
+    });
+
+}
