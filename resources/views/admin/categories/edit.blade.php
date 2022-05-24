92634a6ac1551695a3edea1eb293a38b458a8f60
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-05-24 07:58:31 +0200

    categories edit template

---
+++ resources/views/admin/categories/edit.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.base')
+
+@section('content')
+    <div class="container pt-5">
+        <form action="{{ route('admin.categories.update', $category->id) }}" method="POST">
+            @csrf
+            @method('PUT')
+            <div class="mb-3">
+                <label for="name" class="form-label">{{ __('Name') }}</label>
+                <input type="text" class="form-control" id="name" name="name" value="{{ $category->name }}">
+            </div>
+            <div class="mb-3">
+                <label for="description" class="form-label">{{ __('Description') }}</label>
+                <textarea class="form-control" id="description" rows="10" name="description">{{ $category->description }}</textarea>
+            </div>
+            <input type="submit" class="btn btn-primary">
+        </form>
+    </div>
+@endsection
